cf5fd9f482b5ac91150e012a987496e0d9de0f73
yash <yash@technicalpublications.in>
2024-03-19 00:03:57 -0500

    initial commit

---
+++ src/utils/navigation.ts
@@ -0,0 +1,28 @@
+// An array of links for navigation bar
+export const navBarLinks = [
+  { name: "Home", url: "/" },
+  { name: "Technical Publications", url: "/technical-publications" },
+  { name: "Saniket Builders", url: "/saniket-builders" },
+  { name: "Champ Endurance", url: "/champ-endurance" },
+  { name: "Loha Foundation", url: "/loha-foundation" },
+
+];
+// An array of links for footer
+export const footerLinks = [
+
+  {
+    section: "Company",
+    links: [
+      { name: "About us", url: "#" },
+      { name: "Blog", url: "#" },
+      { name: "Careers", url: "#" },
+      { name: "Customers", url: "#" },
+    ],
+  },
+];
+// An object of links for social icons
+export const socialLinks = {
+  facebook: "#",
+  x: "#",
+  google: "#",
+};
